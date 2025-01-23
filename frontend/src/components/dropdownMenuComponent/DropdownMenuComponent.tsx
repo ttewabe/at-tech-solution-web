fccbc94adbc913716a49b989de4214e83af91dc1
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-23 08:55:24 +0300

    template to put the right side dropdown

--- frontend/src/components/dropdownMenuComponent/DropdownMenuComponent.tsx
+++ frontend/src/components/dropdownMenuComponent/DropdownMenuComponent.tsx
@@ -1,18 +1,47 @@
-import { DropdownMenu } from "../../header/Header.style";
+import {
+    ContactList,
+    DropdownContentContainer,
+    DropdownList,
+    DropdownListItem,
+    DropdownLogoContainer,
+    DropdownMenu,
+    Logo,
+    SocialMediaLogos,
+    SocialMediaLogoItem,
+    CloseButton,
+} from '../../header/Header.style';
 
 // lets take out the dropdown menu from the header and make it a separate component in this file
 const DropdownMenuComponent: React.FC = () => {
     return (
         <DropdownMenu>
-            <ul>
-                <li>Website Development</li>
-                <li>Application Development</li>
-                <li>Business Card Development</li>
-                <li>Teaching Coding</li>
-                <li>Social Services</li>
-            </ul>
+            <CloseButton>X</CloseButton>
+            <DropdownLogoContainer>
+                <Logo>AT Tech</Logo>
+            </DropdownLogoContainer>
+            <DropdownContentContainer>
+                <DropdownList>
+                    <DropdownListItem>Website Development</DropdownListItem>
+                    <DropdownListItem>Social Services</DropdownListItem>
+                    <DropdownListItem>
+                        Business Card Development
+                    </DropdownListItem>
+                    <DropdownListItem>Teaching Coding</DropdownListItem>
+                    <DropdownListItem>Application Development</DropdownListItem>
+                </DropdownList>
+            </DropdownContentContainer>
+            <ContactList>
+                <SocialMediaLogos>
+                    <SocialMediaLogoItem>h</SocialMediaLogoItem>
+                    <SocialMediaLogoItem>y</SocialMediaLogoItem>
+                    <SocialMediaLogoItem>h</SocialMediaLogoItem>
+                    <SocialMediaLogoItem>y</SocialMediaLogoItem>
+                    <SocialMediaLogoItem>h</SocialMediaLogoItem>
+                    <SocialMediaLogoItem>y</SocialMediaLogoItem>
+                </SocialMediaLogos>
+            </ContactList>
         </DropdownMenu>
     );
-}
+};
 
 export default DropdownMenuComponent;
