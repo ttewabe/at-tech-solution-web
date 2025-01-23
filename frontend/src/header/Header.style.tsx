0645e5c1d8f8396ba57f797a27cc1adc82557ad8
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-23 08:56:19 +0300

    styled components to be used to render the logo, close button, header components

--- frontend/src/header/Header.style.tsx
+++ frontend/src/header/Header.style.tsx
@@ -2,17 +2,28 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
     display: flex;
+    flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding: 10px 20px;
     background-color: #003366;
     color: white;
+    padding: 0 0.8rem 0.8rem 1.2rem;
+`;
+
+export const HeaderMainLine = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    padding: 0 20px 0 20px;
     font-family: Arial, sans-serif;
+    gap: 20px;
 `;
 
 export const Logo = styled.div`
     font-size: 24px;
     font-weight: bold;
+    min-width: max-content;
 `;
 
 export const HeaderDetails = styled.div`
@@ -38,9 +49,11 @@ export const ContactInfo = styled.div`
     flex-direction: row;
     align-items: flex-end;
     gap: 20px;
+    margin-left: 5%;
+    width: 100%;
 
     span {
-        font-size: 14px;
+        font-size: 12px;
     }
 `;
 
@@ -57,29 +70,73 @@ export const HamburgerButton = styled.button`
     cursor: pointer;
 `;
 
+export const DropdownLogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem;
+`;
+
+export const CloseButton = styled.div`
+    color: #aa2222;
+    text-align: right;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    border: solid 1px #a2a2a2;
+`;
+
+export const DropdownContentContainer = styled.div`
+    display: flex;
+    justify-content: left;
+    padding-left: 1.5rem;
+`;
+
+export const DropdownList = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
+export const DropdownListItem = styled.li`
+    padding: 10px 20px;
+    margin: 0.8rem;
+    cursor: pointer;
+    background-color: #d9d9d9;
+    back &:hover {
+        background-color: #e8e5e5;
+    }
+`;
+
+export const ContactList = styled.div`
+    display: flex;
+    padding: 5px;
+    margin: 1rem 1rem;
+    border 1px solid #a2a2a2;
+    background-color: #d9d9d9;
+`;
+
+export const SocialMediaLogos = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    gap: 0.3rem;
+    padding: 0.5rem;
+    justify-content: space-between;
+`;
+
+export const SocialMediaLogoItem = styled.div`
+    width: 10px;
+`;
+
 export const DropdownMenu = styled.div`
-    position: absolute;
-    top: 100%;
+    position: fixed;
+    top: 0;
     right: 0;
     background: white;
     color: black;
     border: 1px solid #c9c9c9;
-    border-radius: 4px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+    width: 350px;
+    height: 700px;
     z-index: 1000;
-
-    ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-
-        li {
-            padding: 10px 20px;
-            cursor: pointer;
-
-            &:hover {
-                background-color: #e8e5e5;
-            }
-        }
-    }
 `;
