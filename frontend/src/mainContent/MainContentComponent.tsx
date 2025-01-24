20809bd547f3ded00ed58a7567261508b528768f
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-24 23:33:52 +0300

    company moto content and about section content

--- frontend/src/mainContent/MainContentComponent.tsx
+++ frontend/src/mainContent/MainContentComponent.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { MainContent, WallPaper } from './MainContentComponent.style';
+
+import { MainContent, WallPaper, CompanyMoto, AboutSection, CustomButton, } from './MainContentComponent.style';
 import WallPaperImage from '../assets/wallpaper.jpg';
 
 const MainContentComponent: React.FC = () => {
     return (
         <MainContent>
-            <WallPaper backgroundImage={WallPaperImage}/>
-            <h1>Empowering Ideas With Code</h1>
+            <WallPaper backgroundImage={WallPaperImage}>
+                <CompanyMoto>
+                    <h1>Empowering Ideas With Code</h1>
+                    <p>Crafting Websites, Applications, and Dreams Since March 2023.
+                        Your trusted partner for web development, application solutions,
+                        business presence, and educational services.</p>
+                </CompanyMoto>
+            </WallPaper>
+            <AboutSection>
+                <h1>About AT Tech Solutions</h1>
+                <p>Founded by passionate software engineers in March 2023,
+                    Dream Soft has rapidly evolved into a comprehensive technology
+                    solutions provider. Our expertise spans across web development,
+                    application creation, and social services.</p>
+                <CustomButton>Meet the Team</CustomButton>
+            </AboutSection>
         </MainContent>
     )
 }
