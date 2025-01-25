d150bc3fd7a5e9ae84e75e1966aaea9b177a8cba
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-25 17:40:19 +0300

    the footer files which is common for all authentiated pages created with simple elements

---
+++ frontend/src/footer/Footer.style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+    background-color: #333;
+    color: #fff;
+`;
+
+export const FooterContent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+
---
+++ frontend/src/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterContainer, FooterContent } from './Footer.style';
+
+const Footer: React.FC = () => {
+    return (
+        <FooterContainer>
+            <FooterContent>
+                <p>© 2024 AT Tech. All rights reserved.</p>
+            </FooterContent>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
