9377d4c10b9527aff42621f8d5d13d597dc7c2a1
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-25 21:27:11 +0300

    render the styled components in the footer with the content

--- frontend/src/footer/Footer.tsx
+++ frontend/src/footer/Footer.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { FooterContainer, FooterContent } from './Footer.style';
+import { FooterContainer, FooterContent, FooterMain, FooterBottom, FooterElements } from './Footer.style';
 
 const Footer: React.FC = () => {
     return (
         <FooterContainer>
             <FooterContent>
-                <p>© 2024 AT Tech. All rights reserved.</p>
+                <FooterMain>
+                    <FooterElements>
+                        <h4>Quick Links</h4>
+                        <ul>
+                            <li>Services</li>
+                            <li>Portfolio</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </FooterElements>
+                    <FooterElements>
+                        <h4>Socials</h4>
+                        <ul>
+                            <li>Facebook</li>
+                            <li>Instagram</li>
+                            <li>Twitter</li>
+                        </ul>
+                    </FooterElements>
+                    <FooterElements>
+                        <h3>Newsletter</h3>
+                        <div>
+                            <input type='text' placeholder='Enter your Email' />
+                            <button>Subscribe</button>
+                        </div>
+                    </FooterElements>
+                </FooterMain>
+                <FooterBottom>
+                    <p>© 2024 AT Tech. All rights reserved.</p>
+                </FooterBottom>
             </FooterContent>
         </FooterContainer>
     );
