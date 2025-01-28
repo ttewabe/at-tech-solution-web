b62735dfecf88c429286a1fd5138421132460c5b
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-28 04:54:08 +0300

    empty page and styled component for services page

---
+++ frontend/src/services/Services.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MainContent } from '../mainContent/MainContentComponent.style';
+
+const Services: React.FC = () => {
+    return <MainContent>Services</MainContent>;
+};
+
+export default Services;
