bec3f153d5e98d54c2a57e6d0976f80bf386d40f
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-28 04:53:00 +0300

    empty starter file for portfolio

---
+++ frontend/src/portfolio/Portfolio.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MainContent } from '../mainContent/MainContentComponent.style';
+
+const Portfolio: React.FC = () => {
+    return <MainContent>Portfolio</MainContent>;
+};
+
+export default Portfolio;
