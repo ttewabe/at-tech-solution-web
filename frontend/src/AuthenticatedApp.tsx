931806316091395b1def13462d2260b6b708efdc
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-28 04:56:29 +0300

    routes added for other main navigation items from header

--- frontend/src/AuthenticatedApp.tsx
+++ frontend/src/AuthenticatedApp.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HeaderProvider from './pages/HeaderProvider';
 import MainContentComponent from './mainContent/MainContentComponent';
+import Services from './services/Services';
+import Portfolio from './portfolio/Portfolio';
 
 const AuthenticatedApp: React.FC = () => {
     return (
         <Routes>
             <Route path="" element={<HeaderProvider />}>
                 <Route path="home" element={<MainContentComponent />} />
+                <Route path="services" element={<Services />} />
+                <Route path="portfolio" element={<Portfolio />} />
+                <Route path="contact" element={<Services />} />
+                <Route path="about" element={<Services />} />
             </Route>
         </Routes>
     );
-}
+};
 
 export default AuthenticatedApp;
