20c129a3bc91f74da45702f928b9087a97e1dc76
AbenezerH <abenezer.hailu00@gmail.com>
2025-01-31 05:27:11 +0300

    extract out the content from the component and dynamically insert it

--- frontend/src/components/mainContent/homeContent/HomeContent.tsx
+++ frontend/src/components/mainContent/homeContent/HomeContent.tsx
@@ -18,6 +18,55 @@ import AppDevelopmentIcon from '../../../assets/app-development-icon.png';
 import MobileDevelopmentIcon from '../../../assets/mobile-development-icon.jpg';
 import { FaAngleRight } from 'react-icons/fa';
 
+const services = [
+    {
+        icon: AppDevelopmentIcon,
+        title: 'Web Development',
+        description: 'Custom websites and web applications',
+    },
+    {
+        icon: MobileDevelopmentIcon,
+        title: 'Application Development',
+        description: 'Mobile and Desktop applications',
+    },
+    {
+        icon: EducationIcon,
+        title: 'Educational Services',
+        description: 'Coding classes for all ages',
+    },
+];
+
+const featuredProjects = [
+    {
+        title: 'Enterprise Resource Planning System',
+        description: [
+            'An ERP system integrates business processes into a single system, improving efficiency and decision-making.',
+            'It provides real-time data and insights for better resource management.',
+        ],
+    },
+    {
+        title: 'Inventory Management System',
+        description: [
+            'An Inventory Management System tracks and manages inventory levels, orders, sales, and deliveries.',
+            'It optimizes stock levels, reduces costs, and improves overall efficiency.',
+        ],
+    },
+    {
+        title: 'Insurance Claim Management System',
+        description: [
+            'An Insurance Claim Management System streamlines the process of filing, tracking, and managing insurance claims.',
+            'It enhances efficiency, reduces errors, and improves customer satisfaction.',
+        ],
+    },
+    {
+        title: 'Customer Relationship Management System',
+        description: [
+            'A Customer Relationship Management (CRM) system helps manage interactions with customers and prospects.',
+            'It improves customer service, boosts sales, and enhances customer retention.',
+        ],
+    },
+];
+
 const HomeContent: React.FC = () => {
     return (
         <MainContent>
@@ -46,100 +95,34 @@ const HomeContent: React.FC = () => {
                 </p>
                 <CustomButton>Meet the Team</CustomButton>
                 <ServicesGrid>
-                    <ServiceCard>
-                        <ServiceCardIcon>
-                            <img alt="" src={AppDevelopmentIcon} />
-                        </ServiceCardIcon>
-                        <h4>Web Development</h4>
-                        <p>Custom websites and web applications</p>
-                    </ServiceCard>
-                    <ServiceCard>
-                        <ServiceCardIcon>
-                            <img alt="" src={MobileDevelopmentIcon} />
-                        </ServiceCardIcon>
-                        <h4>Application Development</h4>
-                        <p>Mobile and Desktop applications</p>
-                    </ServiceCard>
-                    <ServiceCard>
-                        <ServiceCardIcon>
-                            <img alt="" src={EducationIcon} />
-                        </ServiceCardIcon>
-                        <h4>Educational Services</h4>
-                        <p>Coding classes for all ages</p>
-                    </ServiceCard>
+                    {services.map((service, index) => (
+                        <ServiceCard key={index}>
+                            <ServiceCardIcon>
+                                <img alt="" src={service.icon} />
+                            </ServiceCardIcon>
+                            <h4>{service.title}</h4>
+                            <p>{service.description}</p>
+                        </ServiceCard>
+                    ))}
                 </ServicesGrid>
             </AboutSection>
             <AboutSection key={'featured-projects'}>
                 <h1>Featured Projects</h1>
                 <p>Explore some of our recent work and success stories</p>
                 <ServicesGrid className="featured-projects">
-                    <ServiceCard className="featured-project-card">
-                        <ServiceCard>
-                            <h4>Enterprise Resource Planning System</h4>
+                    {featuredProjects.map((project, index) => (
+                        <ServiceCard key={index} className="featured-project-card">
+                            <ServiceCard>
+                                <h4>{project.title}</h4>
+                            </ServiceCard>
+                            {project.description.map((desc, i) => (
+                                <p key={i}>{desc}</p>
+                            ))}
+                            <ProjectLink>
+                                View Project <FaAngleRight />
+                            </ProjectLink>
                         </ServiceCard>
-                        <p>
-                            An ERP system integrates business processes into a
-                            single system, improving efficiency and
-                            decision-making.
-                        </p>
-                        <p>
-                            It provides real-time data and insights for better
-                            resource management.
-                        </p>
-                        <ProjectLink>
-                            View Project <FaAngleRight />
-                        </ProjectLink>
-                    </ServiceCard>
-                    <ServiceCard className="featured-project-card">
-                        <ServiceCard>
-                            <h4>Inventory Management System</h4>
-                        </ServiceCard>
-                        <p>
-                            An Inventory Management System tracks and manages
-                            inventory levels, orders, sales, and deliveries.
-                        </p>
-                        <p>
-                            It optimizes stock levels, reduces costs, and
-                            improves overall efficiency.
-                        </p>
-                        <ProjectLink>
-                            View Project <FaAngleRight />
-                        </ProjectLink>
-                    </ServiceCard>
-                    <ServiceCard className="featured-project-card">
-                        <ServiceCard>
-                            <h4>Insurance Claim Management System</h4>
-                        </ServiceCard>
-                        <p>
-                            An Insurance Claim Management System streamlines the
-                            process of filing, tracking, and managing insurance
-                            claims.
-                        </p>
-                        <p>
-                            It enhances efficiency, reduces errors, and improves
-                            customer satisfaction.
-                        </p>
-                        <ProjectLink>
-                            View Project <FaAngleRight />
-                        </ProjectLink>
-                    </ServiceCard>
-                    <ServiceCard className="featured-project-card">
-                        <ServiceCard>
-                            <h4>Customer Relationship Management System</h4>
-                        </ServiceCard>
-                        <p>
-                            A Customer Relationship Management (CRM) system
-                            helps manage interactions with customers and
-                            prospects.
-                        </p>
-                        <p>
-                            It improves customer service, boosts sales, and
-                            enhances customer retention.
-                        </p>
-                        <ProjectLink>
-                            View Project <FaAngleRight />
-                        </ProjectLink>
-                    </ServiceCard>
+                    ))}
                 </ServicesGrid>
             </AboutSection>
         </MainContent>
