09280e41e04051365a4a6970c3ddf10d9acbab0d
AbenezerH <abenezer.hailu00@gmail.com>
2025-02-12 00:27:52 +0300

    add the contact and about pages into the routes

--- frontend/src/AuthenticatedApp.tsx
+++ frontend/src/AuthenticatedApp.tsx
@@ -4,6 +4,8 @@ import HeaderProvider from './components/headerFooterProvider/HeaderFooterProvid
 import HomeContent from './components/mainContent/homeContent/HomeContent';
 import Services from './components/mainContent/services/Services';
 import Portfolio from './components/mainContent/portfolio/Portfolio';
+import Contact from './components/mainContent/contact/Contact';
+import About from './components/mainContent/about/About';
 
 const AuthenticatedApp: React.FC = () => {
     return (
@@ -12,8 +14,8 @@ const AuthenticatedApp: React.FC = () => {
                 <Route path="home" element={<HomeContent />} />
                 <Route path="services" element={<Services />} />
                 <Route path="portfolio" element={<Portfolio />} />
-                <Route path="contact" element={<Services />} />
-                <Route path="about" element={<Services />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="about" element={<About />} />
             </Route>
         </Routes>
     );
