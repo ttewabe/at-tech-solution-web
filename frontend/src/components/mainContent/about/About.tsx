57b335d28429dd6a697f8880bcb3b81a689b2358
AbenezerH <abenezer.hailu00@gmail.com>
2025-02-20 08:59:58 +0300

    use the testimonial data from the data file and also extract out the render logic from the content and dynamically insert it into the page

--- frontend/src/components/mainContent/about/About.tsx
+++ frontend/src/components/mainContent/about/About.tsx
@@ -6,106 +6,114 @@ import {
     SectionTitle,
 } from '../../commonStyledComponents/CommonStyledComponents.style';
 import { Testimonials } from '../../testimonials/Testimonials';
-import clientImage1 from '../../../assets/client-graph.jpg';
-import clientImage2 from '../../../assets/client-buildings.jpg';
+import { testimonials } from '../../../commonData/data';
 
-const testimonials = [
+const sectionContents = [
     {
-        title: 'Happy HR Management',
-        description: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus et felis.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus et felis.',
-        ],
-        image: clientImage1,
+        title: 'Company Overview',
+        content: (
+            <SectionContentP>
+                We are a company dedicated to helping people achieve their goals
+                and dreams through technology. Our mission is to empower ideas
+                with code, offering a range of services including application
+                and web development, coding training, and various social
+                services.
+            </SectionContentP>
+        ),
     },
     {
-        title: 'Global Tours',
-        description: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus et felis.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus et felis.',
-        ],
-        image: clientImage2,
+        title: 'Company History',
+        content: (
+            <SectionContentP>
+                Tewabe and Abenezer founded the company with a shared vision of
+                harnessing the power of technology to transform lives. They
+                started the company to offer application and web development,
+                coding training, and essential social services such as
+                citizenship forms, USIS forms, and notary services.
+            </SectionContentP>
+        ),
+    },
+    {
+        title: 'Core Values',
+        content: (
+            <SectionContentDiv>
+                <ul>
+                    <li>Innovation</li>
+                    <li>Integrity</li>
+                    <li>Customer-Centricity</li>
+                    <li>Collaboration</li>
+                    <li>Excellence</li>
+                </ul>
+            </SectionContentDiv>
+        ),
+    },
+    {
+        title: 'Team Introduction',
+        content: (
+            <SectionContentP>
+                The company currently consists of two members: Tewabe and
+                Abenezer. Tewabe is in charge, and they bring diverse
+                perspectives and experiences from different countries, enriching
+                the company's approach to problem-solving and innovation.
+            </SectionContentP>
+        ),
+    },
+    {
+        title: 'Services and Expertise',
+        content: (
+            <SectionContentDiv>
+                We offer a range of services, including:
+                <ul>
+                    <li>Application and Web Development</li>
+                    <li>Coding Training</li>
+                    <li>
+                        Social Services (citizenship forms, USIS forms, notary
+                        services)
+                    </li>
+                </ul>
+            </SectionContentDiv>
+        ),
+    },
+    {
+        title: 'Corporate Social Responsibility',
+        content: (
+            <SectionContentP>
+                [Share how our company contributes to the community and the
+                causes we support. like basic tech education and training and
+                social services]
+            </SectionContentP>
+        ),
+    },
+    {
+        title: 'Contact Information',
+        content: (
+            <ContactInfo>
+                You can reach us via email or phone number:
+                <ul>
+                    <li>Email: info@attechsolution.com</li>
+                    <li>Phone: +1-123-456-7890</li>
+                </ul>
+            </ContactInfo>
+        ),
     },
 ];
 
 const About: React.FC = () => {
     return (
         <MainWrapper>
-            <Section>
-                <SectionTitle>Company Overview</SectionTitle>
-                <SectionContentP>
-                    We are a company dedicated to helping people achieve their
-                    goals and dreams through technology. Our mission is to
-                    empower ideas with code, offering a range of services
-                    including application and web development, coding training,
-                    and various social services.
-                </SectionContentP>
-            </Section>
-            <Section>
-                <SectionTitle>Company History</SectionTitle>
-                <SectionContentP>
-                    Tewabe and Abenezer founded the company with a shared vision
-                    of harnessing the power of technology to transform lives.
-                    They started the company to offer application and web
-                    development, coding training, and essential social services
-                    such as citizenship forms, USIS forms, and notary services.
-                </SectionContentP>
-            </Section>
-            <Section>
-                <SectionTitle>Core Values</SectionTitle>
-                <SectionContentDiv>
-                    <ul>
-                        <li>Innovation</li>
-                        <li>Integrity</li>
-                        <li>Customer-Centricity</li>
-                        <li>Collaboration</li>
-                        <li>Excellence</li>
-                    </ul>
-                </SectionContentDiv>
-            </Section>
-            <Section>
-                <SectionTitle>Team Introduction</SectionTitle>
-                <SectionContentP>
-                    The company currently consists of two members: Tewabe and
-                    Abenezer. Tewabe is in charge, and they bring diverse
-                    perspectives and experiences from different countries,
-                    enriching the company's approach to problem-solving and
-                    innovation.
-                </SectionContentP>
-            </Section>
-            <Section>
-                <SectionTitle>Services and Expertise</SectionTitle>
-                <SectionContentDiv>
-                    We offer a range of services, including:
-                    <ul>
-                        <li>Application and Web Development</li>
-                        <li>Coding Training</li>
-                        <li>
-                            Social Services (citizenship forms, USIS forms,
-                            notary services)
-                        </li>
-                    </ul>
-                </SectionContentDiv>
-            </Section>
-            <Testimonials testimonials={testimonials} />
-            <Section>
-                <SectionTitle>Corporate Social Responsibility</SectionTitle>
-                <SectionContentP>
-                    [Share how our company contributes to the community and the
-                    causes we support. like basic tech education and training
-                    and social services]
-                </SectionContentP>
-            </Section>
-            <Section>
-                <SectionTitle>Contact Information</SectionTitle>
-                <ContactInfo>
-                    You can reach us via email or phone number:
-                    <ul>
-                        <li>Email: info@attechsolution.com</li>
-                        <li>Phone: +1-123-456-7890</li>
-                    </ul>
-                </ContactInfo>
-            </Section>
+            {sectionContents.map((section, index) => (
+                <Section key={index}>
+                    <SectionTitle>{section.title}</SectionTitle>
+                    {section.content}
+                </Section>
+            ))}
+            <Testimonials
+                testimonials={testimonials.map((testimonial) => ({
+                    title: testimonial.company,
+                    description: [testimonial.quote, testimonial.author],
+                    image: testimonial.image,
+                }))}
+            />
         </MainWrapper>
     );
 };
